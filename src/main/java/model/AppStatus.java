a634b62a247b5b84ac635dc0fcec2ea9ed20ee29
sarafidi <sarafina.rafidi@student.mmu.edu.my>
2026-06-06 15:08:54 +0800

    [create] Enum Role & AppStatus class

---
+++ src/main/java/model/AppStatus.java
@@ -0,0 +1,5 @@
+package model;
+
+public enum AppStatus {
+    SCHEDULED, CONFIRMED, COMPLETED, CANCELLED;
+}
---
+++ src/main/java/model/Role.java
@@ -0,0 +1,5 @@
+package model;
+
+public enum Role {
+    ADMIN, RECEPTIONIST, DOCTOR;
+}
