af5de389df18bab2da1d728bc8c4afca9e152e5e
sarafidi <sarafina.rafidi@student.mmu.edu.my>
2026-06-08 09:59:47 +0800

    [create] Utility class - SessionManager.java

---
+++ src/main/java/util/SessionManager.java
@@ -0,0 +1,33 @@
+package util;
+
+import model.User;
+
+public class SessionManager {
+    private static SessionManager instance;
+    private User currentUser;
+
+    private SessionManager() {}
+
+    public static SessionManager getInstance() {
+        if (instance == null) {
+            instance = new SessionManager();
+        }
+        return instance;
+    }
+
+    public void setCurrentUser(User currentUser) {
+        this.currentUser = currentUser;
+    }
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public boolean isLoggedIn() {
+        return currentUser != null;
+    }
+
+    public void clearSession() {
+        currentUser = null;
+    }
+}
